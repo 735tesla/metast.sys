6ffb91c0640a6297c46ef710a6b144bed5afde89
Henry Pitcairn <735tesla@gmail.com>
2016-02-27 16:05:10 -0500

    create project

---
+++ users.sys/users.sys/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  users.sys
+//
+//  Created by Nikola Tesla on 2/27/16.
+//  Copyright © 2016 WireTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
