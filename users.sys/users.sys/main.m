abb09a9ea802f4c6acd1e065e146597244acbc62
Henry Pitcairn <735tesla@gmail.com>
2016-02-27 17:25:21 -0500

    users

--- users.sys/users.sys/main.m
+++ users.sys/users.sys/main.m
@@ -7,11 +7,36 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <OpenDirectory/OpenDirectory.h>
+#include <err.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        ODSession *s = [ODSession defaultSession];
+        ODNode *root = [ODNode nodeWithSession:s name:@"/Local/Default" error:nil];
+        ODQuery *usersQuery = [ODQuery queryWithNode:root
+                                      forRecordTypes:kODRecordTypeUsers
+                                           attribute:kODAttributeTypeAllAttributes
+                                           matchType:0
+                                         queryValues:nil
+                                     returnAttributes:nil
+                                      maximumResults:0 error:nil];
+        NSArray *users = [usersQuery resultsAllowingPartial:NO error:nil];
+        for (ODRecord *user in users) {
+            NSString *userName = [user recordName];
+            struct passwd *userPwd = getpwnam([userName UTF8String]);
+            if (userPwd->pw_uid < 500 || userPwd->pw_uid >= 4294967294)
+                continue;
+            NSString *fullName = [user valuesForAttribute:kODAttributeTypeFullName error:nil][0];
+            printf("%s : %s\n", [userName UTF8String], [fullName UTF8String]);
+        }
     }
     return 0;
 }
